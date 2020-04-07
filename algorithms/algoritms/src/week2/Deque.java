8c8bb5eb59e83bd4817f00ccea37d22ef6f98f0f
mardikar <saurabh845292@gmail.com>
2020-04-08 04:22:32 +0530

    change hasNext

--- algorithms/algoritms/src/week2/Deque.java
+++ algorithms/algoritms/src/week2/Deque.java
@@ -5,140 +5,141 @@ import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
 
-	private class Node {
-		public Item item;
-		public Node next;
+    private class Node {
+        public Item item;
+        public Node next;
 
-		Node(Item item) {
-			this.item = item;
-			next = null;
-		}
+        Node(Item item) {
+            this.item = item;
+            next = null;
+        }
 
-	}
+    }
 
-	private int length = 0;
-	private Node first = null, last = null;
-	private final IllegalArgumentException illegalArgumentException = new IllegalArgumentException();
-	private final NoSuchElementException noSuchElementException = new NoSuchElementException();
+    private int length = 0;
+    private Node first = null, last = null;
+    private final IllegalArgumentException illegalArgumentException = new IllegalArgumentException();
+    private final NoSuchElementException noSuchElementException = new NoSuchElementException();
 
-	public Deque() {
+    public Deque() {
 
-	}
+    }
 
-	public boolean isEmpty() {
-		return length == 0;
-	}
+    public boolean isEmpty() {
+        return length == 0;
+    }
 
-	public int size() {
-		return length;
-	}
+    public int size() {
+        return length;
+    }
 
-	private void validateInput(Item item) {
-		if (item == null) {
-			throw illegalArgumentException;
-		}
-	}
+    private void validateInput(Item item) {
+        if (item == null) {
+            throw illegalArgumentException;
+        }
+    }
 
-	public void addFirst(Item item) {
-		validateInput(item);
-		if (isEmpty()) {
-			first = new Node(item);
-			last = first;
-		} else {
-			Node oldFirst = first;
-			first = new Node(item);
-			first.next = oldFirst;
-		}
-		length++;
-	}
+    public void addFirst(Item item) {
+        validateInput(item);
+        if (isEmpty()) {
+            first = new Node(item);
+            last = first;
+        } else {
+            Node oldFirst = first;
+            first = new Node(item);
+            first.next = oldFirst;
+        }
+        length++;
+    }
 
-	public void addLast(Item item) {
-		validateInput(item);
-		if (isEmpty()) {
-			first = new Node(item);
-			last = first;
-		} else {
-			Node oldLast = last;
-			last = new Node(item);
-			oldLast.next = last;
-		}
-		length++;
-	}
+    public void addLast(Item item) {
+        validateInput(item);
+        if (isEmpty()) {
+            first = new Node(item);
+            last = first;
+        } else {
+            Node oldLast = last;
+            last = new Node(item);
+            oldLast.next = last;
+        }
+        length++;
+    }
 
-	private void validateRemove() {
-		if (isEmpty()) {
-			throw noSuchElementException;
-		}
-	}
+    private void validateRemove() {
+        if (isEmpty()) {
+            throw noSuchElementException;
+        }
+    }
 
-	public Item removeFirst() {
-		validateRemove();
-		Item item = first.item;
-		first = first.next;
-		length--;
-		return item;
-	}
+    public Item removeFirst() {
+        validateRemove();
+        Item item = first.item;
+        first = first.next;
+        length--;
+        return item;
+    }
 
-	public Item removeLast() {
-		validateRemove();
-		Item item = last.item;
-		Node pointer = first;
-		while (pointer.next != last)
-			pointer = pointer.next;
-		last = pointer;
-		last.next = null;
-		length--;
-		return item;
-	}
+    public Item removeLast() {
+        validateRemove();
+        Item item = last.item;
+        Node pointer = first;
+        while (pointer.next != null && pointer.next != last)
+            pointer = pointer.next;
+        last = pointer;
+        last.next = null;
+        length--;
+        return item;
+    }
 
-	public Iterator<Item> iterator() {
-		return new DequeIterator();
-	}
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
 
-	private class DequeIterator implements Iterator<Item> {
+    private class DequeIterator implements Iterator<Item> {
 
-		private Node current = first;
-		private final UnsupportedOperationException unsupportedOperationException = new UnsupportedOperationException();
+        private Node current = first;
+        private final UnsupportedOperationException unsupportedOperationException = new UnsupportedOperationException();
 
-		public boolean hasNext() {
-			return current != null;
-		}
+        public boolean hasNext() {
+            return current.next != null;
+        }
 
-		public Item next() {
-			if (!hasNext()) {
-				throw noSuchElementException;
-			}
-			Item item = current.item;
-			current = current.next;
-			return item;
-		}
+        public Item next() {
+            if (!hasNext()) {
+                throw noSuchElementException;
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
 
-		public void remove() {
-			throw unsupportedOperationException;
-		}
+        public void remove() {
+            throw unsupportedOperationException;
+        }
 
-	}
+    }
 
-	private void printDeque() {
-		for (Item s : this) {
-			System.out.println(s);
-		}
-	}
+    private void printDeque() {
+        for (Item s : this) {
+            System.out.println(s);
+        }
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Deque<String> deque = new Deque<String>();
-		deque.addFirst("Saurabh");
-		deque.printDeque();
-		deque.addFirst("Sukanya");
-		deque.printDeque();
-		deque.addLast("Mardikar");
-		deque.printDeque();
-		deque.removeFirst();
-		deque.printDeque();
-		System.out.println(deque.removeLast());
-		deque.printDeque();
+        Deque<String> deque = new Deque<String>();
+        deque.addFirst("Saurabh");
+        deque.printDeque();
+        deque.removeLast();
+        deque.addFirst("Sukanya");
+        deque.printDeque();
+        deque.addLast("Mardikar");
+        deque.printDeque();
+        deque.removeFirst();
+        deque.printDeque();
+        System.out.println(deque.removeLast());
+        deque.printDeque();
 
-	}
+    }
 
 }

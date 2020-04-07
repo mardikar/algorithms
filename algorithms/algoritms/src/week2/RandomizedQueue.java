4493af588df0271b047fb3dfed2fb982b852f77d
mardikar <saurabh845292@gmail.com>
2020-04-08 04:53:55 +0530

    randomize it

--- algorithms/algoritms/src/week2/RandomizedQueue.java
+++ algorithms/algoritms/src/week2/RandomizedQueue.java
@@ -7,106 +7,112 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-	private int length = 0, capacity = 1;
-	private Item[] queue;
-	private final NoSuchElementException noSuchElementException = new NoSuchElementException();
-	private final IllegalArgumentException illegalArgumentException = new IllegalArgumentException();
+    private int length = 0, capacity = 1;
+    private Item[] queue;
+    private final NoSuchElementException noSuchElementException = new NoSuchElementException();
+    private final IllegalArgumentException illegalArgumentException = new IllegalArgumentException();
 
-	public RandomizedQueue() {
-		queue = (Item[]) new Object[capacity];
-	}
+    public RandomizedQueue() {
+        queue = (Item[]) new Object[capacity];
+    }
 
-	private void resize(int newCapacity) {
-		capacity = newCapacity;
-		Item[] newQueue = (Item[]) new Object[capacity];
-		for (int i = 0; i < length; i++) {
-			newQueue[i] = queue[i];
-		}
-		queue = newQueue;
-	}
+    private void resize(int newCapacity) {
+        capacity = newCapacity;
+        Item[] newQueue = (Item[]) new Object[capacity];
+        for (int i = 0; i < length; i++) {
+            newQueue[i] = queue[i];
+        }
+        queue = newQueue;
+    }
 
-	public boolean isEmpty() {
-		return length == 0;
-	}
+    public boolean isEmpty() {
+        return length == 0;
+    }
 
-	public int size() {
-		return length;
-	}
+    public int size() {
+        return length;
+    }
 
-	private void validateInput(Item item) {
-		if (item == null) {
-			throw illegalArgumentException;
-		}
-	}
+    private void validateInput(Item item) {
+        if (item == null) {
+            throw illegalArgumentException;
+        }
+    }
 
-	public void enqueue(Item item) {
-		validateInput(item);
-		if (capacity == length) {
-			resize(2 * capacity);
-		}
-		queue[length++] = item;
-	}
+    public void enqueue(Item item) {
+        validateInput(item);
+        if (capacity == length) {
+            resize(2 * capacity);
+        }
+        queue[length++] = item;
+    }
 
-	private void validateRemove() {
-		if (isEmpty()) {
-			throw noSuchElementException;
-		}
-	}
+    private void validateRemove() {
+        if (isEmpty()) {
+            throw noSuchElementException;
+        }
+    }
 
-	public Item dequeue() {
-		validateRemove();
-		if (length == capacity / 4) {
-			resize(capacity / 2);
-		}
-		int index = StdRandom.uniform(0, length);
-		Item item = queue[index];
-		queue[index] = queue[--length];
-		return item;
-	}
+    public Item dequeue() {
+        validateRemove();
+        if (length == capacity / 4) {
+            resize(capacity / 2);
+        }
+        int index = StdRandom.uniform(0, length);
+        Item item = queue[index];
+        queue[index] = queue[--length];
+        return item;
+    }
 
-	public Item sample() {
-		return queue[StdRandom.uniform(0, length)];
-	}
+    public Item sample() {
+        validateRemove();
+        return queue[StdRandom.uniform(0, length)];
+    }
 
-	public Iterator<Item> iterator() {
-		return new RandomQueueIterator();
-	}
+    public Iterator<Item> iterator() {
+        return new RandomQueueIterator();
+    }
 
-	private class RandomQueueIterator implements Iterator<Item> {
+    private class RandomQueueIterator implements Iterator<Item> {
 
-		private int current = 0;
-		private final UnsupportedOperationException unsupportedOperationException = new UnsupportedOperationException();
+        private int current = 0;
+        private final int[] indices;
+        private final UnsupportedOperationException unsupportedOperationException = new UnsupportedOperationException();
 
-		public RandomQueueIterator() {
-			StdRandom.shuffle(queue);
-		}
+        public RandomQueueIterator() {
+            indices = new int[length];
+            for (int i = 0; i < length; i++) {
+                indices[i] = i;
+            }
+            StdRandom.shuffle(indices);
+        }
 
-		public boolean hasNext() {
-			return current != length;
-		}
+        public boolean hasNext() {
+            return current != length;
+        }
 
-		public Item next() {
-			if (!hasNext()) {
-				throw noSuchElementException;
-			}
-			return queue[current++];
-		}
+        public Item next() {
+            if (!hasNext()) {
+                throw noSuchElementException;
+            }
+            return queue[indices[current++]];
+        }
 
-		public void remove() {
-			throw unsupportedOperationException;
-		}
+        public void remove() {
+            throw unsupportedOperationException;
+        }
 
-	}
+    }
 
-	public static void main(String[] args) {
-		RandomizedQueue<String> queue = new RandomizedQueue<String>();
-		queue.enqueue("Saurabh");
-		System.out.println(queue.dequeue());
-		queue.enqueue("Saurabh");
-		queue.enqueue("Mardikar");
-		System.out.println(queue.sample());
-		System.out.println(queue.sample());
-		System.out.println(queue.sample());
-	}
+    public static void main(String[] args) {
+        RandomizedQueue<String> queue = new RandomizedQueue<String>();
+        queue.enqueue("Saurabh");
+        System.out.println(queue.dequeue());
+        queue.enqueue("Saurabh");
+        queue.enqueue("Mardikar");
+        System.out.println(queue.sample());
+        System.out.println(queue.sample());
+        System.out.println(queue.sample());
+    }
 
 }
